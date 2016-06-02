33b36effd1f9b83b0ea1d24f7d049c87a118bac4
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-06-01 20:49:10 -0700

    GUI

--- Version2.py
+++ Version2.py
@@ -22,8 +22,8 @@ def addToInventory(item):
 
 canvas = Tkinter.Canvas(root, height = 200, width = 900, relief = Tkinter.RAISED, bg= 'black')
 canvas.grid()
-#photo = Tkinter.PhotoImage(file = "C:\Users\Esmeralda\Pictures\game pic.gif")
-#canvas.create_image(0,0, image = photo)
+photo = Tkinter.PhotoImage(file = "C:\Users\Esmeralda\Pictures\game pic.gif")
+canvas.create_image(0,0, image = photo)
 canvas2 = Tkinter.Canvas(root, height = 350, width = 900, relief = Tkinter.RAISED, bg = 'red')
 canvas2.grid()
 
