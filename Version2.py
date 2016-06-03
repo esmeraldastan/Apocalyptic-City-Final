554ae423627fa5ccbe5e709052bd4dadd81de9f5
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-06-02 19:59:52 -0700

    GUI
    6/02

--- Version2.py
+++ Version2.py
@@ -60,47 +60,47 @@ def pressed(x):
 #-------------------------------------------------WRONG WAY--------------------------------------------------------------------- 
 
 def wrong_way():
-    root5 = Tkinter.Tk()
-    root5.title('WRONG WAY')
+    root = Tkinter.Tk()
+    root.title('WRONG WAY')
     
-    canvas = Tkinter.Canvas(root5, height = 200, width = 900, relief = Tkinter.RAISED, bg= 'black')
+    canvas = Tkinter.Canvas(root, height = 200, width = 900, relief = Tkinter.RAISED, bg= 'black')
     canvas.grid()
     
     message = canvas.create_text(300, 100, text = ' You can\'t go that way!',fill = 'red', font = ('Yu Gothic', -50))
     print message 
     
 #---------------------------------------------------OFFICE------------------------------------------------------------  
-  
+#2
+
 def newWindow():
     #global message
-    root2 = Tkinter.Tk()
-    root2.title('Room: Office')
+    root1 = Tkinter.Tk()
+    root.title('Room: Office')
     
-    canvas = Tkinter.Canvas(root2, height = 100, width = 990, relief = Tkinter.RAISED, bg= 'black')
+    canvas = Tkinter.Canvas(root1, height = 100, width = 990, relief = Tkinter.RAISED, bg= 'black')
     canvas.grid()
-    canvas2 = Tkinter.Canvas(root2, height = 390, width = 990, relief = Tkinter.RAISED, bg = 'red')
+    canvas2 = Tkinter.Canvas(root1, height = 390, width = 990, relief = Tkinter.RAISED, bg = 'red')
     canvas2.grid()
     
-    message1 = canvas.create_text(100, 60, text = 'Room: Office', fill = 'white', font = ('Yu Gothic', -30))
+    message1 = canvas.create_text(300, 60, text = 'Room: Office', fill = 'white', font = ('Yu Gothic', -30))
     print message1  
     message = canvas2.create_text(400, 200, text = 'Papers are shattered everywhere. The lights\nare flashing on and off. Next to you is a\nblue paper.\n\nIt reads: *Escape to the labatory hidden under\nan old facotry building.It should be located a\ncouple of blocks west of where you are located.*\n\nHead north or east', fill = 'black', font = ('Yu Gothic', -30))
     print message 
     
-    button = Tkinter.Button(root2, text= 'North', width = 10, height = 5, command = con_room)
+    button = Tkinter.Button(root1, text= 'North', width = 10, height = 5, command = con_room)
     button.grid(row = 1, column= 4)
     
-
-    button = Tkinter.Button(root2, text= 'East', width = 10, height = 5, command = desk)
+    button = Tkinter.Button(root1, text= 'East', width = 10, height = 5, command = desk)
     button.grid(row = 1, column= 5)
 
-    button = Tkinter.Button(root2, text= 'South', width = 10, height = 5, command = wrong_way)
+    button = Tkinter.Button(root1, text= 'South', width = 10, height = 5, command = wrong_way)
     button.grid(row = 3, column= 4)
 
-    button = Tkinter.Button(root2, text= 'West', width = 10, height = 5, command = wrong_way)
+    button = Tkinter.Button(root1, text= 'West', width = 10, height = 5, command = wrong_way)
     button.grid(row = 3, column= 5)
 
-    button = Tkinter.Button(root2, text= 'QUIT', width = 10, height = 5, command = root2.destroy)
-    button.grid(row = 5, column= 4)
+    button = Tkinter.Button(root1, text= 'QUIT', width = 10, height = 5, command = root1.destroy)
+    button.grid(row = 1, column= 6)
     
 #---------------------------------------------------------INTRO------------------------------------------------------------------  
 #START OF THE GAME 
@@ -112,18 +112,19 @@ button = Tkinter.Button(root, text= 'QUIT', width = 10, height = 5, command = ro
 button.grid(row = 0, column= 2)
 
 #------------------------------------------------------------ELEVATOR/NORTH---------------------------------------------------------------------- 
+#3
 
 def con_room():    
-    root3 = Tkinter.Tk()
-    root3.title('Room: Conference Room')
-    editor3 = Tkinter.Text(master = root3, width= 45, height = 0)
-    editor3.grid( row= 0 , column = 0, sticky = (Tkinter.N, Tkinter.W, Tkinter.E))
-    editor3.insert(Tkinter.END,"add?")
-    editor3.see(Tkinter.END)
+    root2 = Tkinter.Tk()
+    root.title('Room: Conference Room')
+    editor = Tkinter.Text(master = root2, width= 45, height = 0)
+    editor.grid( row= 0 , column = 0, sticky = (Tkinter.N, Tkinter.W, Tkinter.E))
+    editor.insert(Tkinter.END,"add?")
+    editor.see(Tkinter.END)
     
-    canvas = Tkinter.Canvas(root3, height = 100, width = 990, relief = Tkinter.RAISED, bg= 'black')
+    canvas = Tkinter.Canvas(root2, height = 100, width = 990, relief = Tkinter.RAISED, bg= 'black')
     canvas.grid()
-    canvas2 = Tkinter.Canvas(root3, height = 390, width = 990, relief = Tkinter.RAISED, bg = 'red')
+    canvas2 = Tkinter.Canvas(root2, height = 390, width = 990, relief = Tkinter.RAISED, bg = 'red')
     canvas2.grid()
     
     
@@ -133,64 +134,61 @@ def con_room():
     print message1 
     
     
-    if editor3 == "add":
+    if editor == "add":
         addToInventory(input)
         print(inventory)
     print 
     
     #BUTTONS "LOCATIONS"
 
-    button = Tkinter.Button(root3, text= 'North', width = 10, height = 5, command = newWindow3)
+    button = Tkinter.Button(root2, text= 'North', width = 10, height = 5, command = newWindow3)
     button.grid(row = 0, column= 5)
     
-    button = Tkinter.Button(root3, text= 'East', width = 10, height = 5, command = elevator)
+    button = Tkinter.Button(root2, text= 'East', width = 10, height = 5, command = elevator)
     button.grid(row = 0, column= 5)
 
-    button = Tkinter.Button(root3, text= 'South', width = 10, height = 5, command = newWindow3)
+    button = Tkinter.Button(root2, text= 'South', width = 10, height = 5, command = newWindow3)
     button.grid(row = 3, column= 4)
 
-    button = Tkinter.Button(root3, text= 'West', width = 10, height = 5, command = wrong_way)
+    button = Tkinter.Button(root2, text= 'West', width = 10, height = 5, command = wrong_way)
     button.grid(row = 2, column= 3)
 
-    button = Tkinter.Button(root3, text= 'QUIT', width = 10, height = 5, command = root3.destroy3)
+    button = Tkinter.Button(root2, text= 'QUIT', width = 10, height = 5, command = root2.destroy)
     button.grid(row = 5, column= 4)
     
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def desk():    
-    root6 = Tkinter.Tk()
-    editor6 = Tkinter.Text(master = root6, width = 45, height = 0)
-    editor6.grid(row = 0, colum =0, sticky = (Tkinter.N, Tkinter.W, Tkinter.E))
-    editor6.insert(Tkinter.End, "Room : Elevator")
-    editor6.see(Tkinter.End)
+    root3 = Tkinter.Tk()
+    root3.title('Location: Secutary Desk')
     
-    canvas = Tkinter.Canvas(root6, height = 200, width = 900, relief = Tkinter.RAISED, bg= 'black')
+    canvas = Tkinter.Canvas(root3, height = 100, width = 990, relief = Tkinter.RAISED, bg= 'black')
     canvas.grid()
-    #photo = Tkinter.PhotoImage(file = "C:\Users\Esmeralda\Pictures\game pic.gif")
-    #canvas.create_image(0,0, image = photo)
-    canvas2 = Tkinter.Canvas(root6, height = 350, width = 500, relief = Tkinter.RAISED, bg = 'red')
+    canvas2 = Tkinter.Canvas(root3, height = 350, width = 900, relief = Tkinter.RAISED, bg = 'red')
     canvas2.grid()
     
-    message = canvas2.create_text(300, 100, text = 'Papers are shattered everywhere. The lights are flashing on and off. Next to you is a blue paper.\n\nType in "pick up" to read what it says.', fill = 'green', font = ('Yu Gothic', -50))
-    print message 
+    message = canvas.create_text(300, 60, text = 'Location: Secutrary Desk', fill = 'white', font = ('Yu Gothic', -30))
+    print message
+    message2 = canvas2.create_text(100, 300, text = ' You are standing next to your securary\'s desk. A "flashlight stands on top. Pick it up you might need it later on.\n\nType\n>add\n\nAfter head either "north" to the elevator or "east" to the stairs.', fill = 'black', font = ('Toxico', -50))
+    print message2 
 
 
     
     #BUTTONS "LOCATIONS"
 
-    button = Tkinter.Button(root6, text= 'North', width = 10, height = 5, command = newWindow)
+    button = Tkinter.Button(root3, text= 'North', width = 10, height = 5, command = newWindow)
     button.grid(row = 0, column= 5)
     
-    button = Tkinter.Button(root6, text= 'East', width = 10, height = 5, command = newWindow)
+    button = Tkinter.Button(root3, text= 'East', width = 10, height = 5, command = newWindow)
     button.grid(row = 0, column= 5)
 
-    button = Tkinter.Button(root6, text= 'South', width = 10, height = 5, command = newWindow)
+    button = Tkinter.Button(root3, text= 'South', width = 10, height = 5, command = newWindow)
     button.grid(row = 3, column= 4)
 
-    button = Tkinter.Button(root6, text= 'West', width = 10, height = 5, command = newWindow)
+    button = Tkinter.Button(root3, text= 'West', width = 10, height = 5, command = newWindow)
     button.grid(row = 2, column= 3)
 
-    button = Tkinter.Button(root6, text= 'QUIT', width = 10, height = 5, command = root6.destroy)
+    button = Tkinter.Button(root3, text= 'QUIT', width = 10, height = 5, command = root3.destroy)
     button.grid(row = 5, column= 4)
 
 def newWindow3():    
